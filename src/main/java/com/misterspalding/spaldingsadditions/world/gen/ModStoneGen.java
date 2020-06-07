476e274bebcddefc67d64e0c060a24d9cd6a65d2
Spalding004 <mclain.harrison@gmail.com>
2020-06-07 11:55:32 -0500

    adjust new stone spawning to 1.12 values

--- src/main/java/com/misterspalding/spaldingsadditions/world/gen/ModStoneGen.java
+++ src/main/java/com/misterspalding/spaldingsadditions/world/gen/ModStoneGen.java
@@ -21,35 +21,43 @@ public class ModStoneGen {
 		
 		for (Biome biome : ForgeRegistries.BIOMES) {
 			
-			ConfiguredPlacement<CountRangeConfig> config = Placement.COUNT_RANGE.configure(new CountRangeConfig(7, 20, 0, 120));
-			ConfiguredPlacement<CountRangeConfig> config_pumice = Placement.COUNT_RANGE.configure(new CountRangeConfig(5, 20, 0, 120));
-			ConfiguredPlacement<CountRangeConfig> config_shale = Placement.COUNT_RANGE.configure(new CountRangeConfig(7, 20, 0, 70));
+			ConfiguredPlacement<CountRangeConfig> config_marcasite = Placement.COUNT_RANGE.configure(new CountRangeConfig(7, 1, 0, 120));
+			ConfiguredPlacement<CountRangeConfig> config_apatite = Placement.COUNT_RANGE.configure(new CountRangeConfig(7, 1, 0, 120));
+			ConfiguredPlacement<CountRangeConfig> config_pumice = Placement.COUNT_RANGE.configure(new CountRangeConfig(7, 1, 0, 55));
+			ConfiguredPlacement<CountRangeConfig> config_shale = Placement.COUNT_RANGE.configure(new CountRangeConfig(20, 10, 0, 120));
+			ConfiguredPlacement<CountRangeConfig> config_shale2 = Placement.COUNT_RANGE.configure(new CountRangeConfig(10, 10, 0, 90));
 			ConfiguredPlacement<CountRangeConfig> config_vendar = Placement.COUNT_RANGE.configure(new CountRangeConfig(3, 12, 0, 40));
 			ConfiguredPlacement<CountRangeConfig> config_vendar2 = Placement.COUNT_RANGE.configure(new CountRangeConfig(200, 12, 0, 30));
-			ConfiguredPlacement<CountRangeConfig> config_umber = Placement.COUNT_RANGE.configure(new CountRangeConfig(7, 20, 0, 120));
-			ConfiguredPlacement<CountRangeConfig> config_alunite = Placement.COUNT_RANGE.configure(new CountRangeConfig(7, 20, 0, 120));
-			ConfiguredPlacement<CountRangeConfig> config_cormalite = Placement.COUNT_RANGE.configure(new CountRangeConfig(7, 20, 0, 120));
+			ConfiguredPlacement<CountRangeConfig> config_umber = Placement.COUNT_RANGE.configure(new CountRangeConfig(12, 40, 0, 180));
+			ConfiguredPlacement<CountRangeConfig> config_alunite = Placement.COUNT_RANGE.configure(new CountRangeConfig(7, 90, 0, 180));
+			ConfiguredPlacement<CountRangeConfig> config_cormalite = Placement.COUNT_RANGE.configure(new CountRangeConfig(7, 20, 0, 95));
 			
 			if(biome == Biomes.RIVER || biome == Biomes.OCEAN) {
 				biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, 
 						Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, 
-								BlockDec.SHALE.get().getDefaultState(), 20)).withPlacement(config_shale));
+								BlockDec.SHALE.get().getDefaultState(), 33)).withPlacement(config_shale));
+				biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, 
+						Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, 
+								BlockDec.SHALE.get().getDefaultState(), 3)).withPlacement(config_shale2));
 			}
 			//																				rarity, y-min, y-max offset, y-max???
 			
 			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, 
 					Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, 
-							BlockDec.APATITE.get().getDefaultState(), 33)).withPlacement(config));
+							BlockDec.APATITE.get().getDefaultState(), 33)).withPlacement(config_apatite));
 			
 			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, 
 					Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, 
-							BlockDec.PUMICE.get().getDefaultState(), 20)).withPlacement(config_pumice));
+							BlockDec.PUMICE.get().getDefaultState(), 10)).withPlacement(config_pumice));
 						
 			
 			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, 
 					Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, 
-							BlockDec.MARCASITE.get().getDefaultState(), 33)).withPlacement(config));
+							BlockDec.MARCASITE.get().getDefaultState(), 33)).withPlacement(config_marcasite));
 			
+			biome.addFeature(GenerationStage.Decoration.SURFACE_STRUCTURES, 
+					Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK, 
+							BlockDec.UMBER.get().getDefaultState(), 33)).withPlacement(config_umber));
 			biome.addFeature(GenerationStage.Decoration.SURFACE_STRUCTURES, 
 					Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK, 
 							BlockDec.UMBER.get().getDefaultState(), 33)).withPlacement(config_umber));
